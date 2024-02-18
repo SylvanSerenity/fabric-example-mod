e491763fdc4ad4eeea35287061d98f4cada118f7
Avery Aaron <sylvanserene@gmail.com>
2024-02-18 10:40:42 -0600

    Track hauntLevel and hauntChance

--- src/main/java/com/sylvan/presence/data/PlayerData.java
+++ src/main/java/com/sylvan/presence/data/PlayerData.java
@@ -5,10 +5,12 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import com.google.gson.JsonObject;
 import com.sylvan.presence.Presence;
 import com.sylvan.presence.event.AmbientSounds;
+import com.sylvan.presence.event.Events;
 import com.sylvan.presence.event.ExtinguishTorches;
 import com.sylvan.presence.event.Footsteps;
 import com.sylvan.presence.event.NearbySounds;
@@ -27,8 +29,10 @@ public class PlayerData {
 	// Config
 	public static float defaultHauntChance = 0.1f;			// Default chance of being haunted when joining the server. Range: [0.0, 1.0]
 	private static float defaultHauntLevel = 1.0f;			// The default haunt level of each player
-	private static float hauntChanceMaxBeforeDecrease = 0.75f;	// The maximum haunt chance before haunted players start getting their haunt chance reduced based on play time
-	private static int minutesToFullyReduceHauntChance = 600;	// The number of minutes that would reduce haunt chance by 100%
+	private static float hauntChanceMaxBeforeReset = 0.8f;		// The maximum haunt chance before haunted players get their haunt chance reset
+	private static int minutesToFullyReduceHauntChance = 60 * 24;	// The playtime in minutes that would reduce haunt chance by 100%
+	private static int minutesToIncreaseHauntLevelBy1 = 60 * 2;	// The playtime of minutes that would increase haunt level by 1.0f
+	private static int hauntLevelCalculationDelay = 60;		// The time between haunt level calculations based on playtime
 
 	public static PlayerData addPlayerData(final PlayerEntity player) {
 		final PlayerData playerData = new PlayerData(player);
@@ -45,7 +49,10 @@ public class PlayerData {
 		try {
 			defaultHauntChance = Presence.config.getOrSetValue("defaultHauntChance", defaultHauntChance).getAsFloat();
 			defaultHauntLevel = Presence.config.getOrSetValue("defaultHauntLevel", defaultHauntLevel).getAsFloat();
-			hauntChanceMaxBeforeDecrease = Presence.config.getOrSetValue("hauntChanceMaxBeforeDecrease", hauntChanceMaxBeforeDecrease).getAsFloat();
+			hauntChanceMaxBeforeReset = Presence.config.getOrSetValue("hauntChanceMaxBeforeReset", hauntChanceMaxBeforeReset).getAsFloat();
+			minutesToFullyReduceHauntChance = Presence.config.getOrSetValue("minutesToFullyReduceHauntChance", minutesToFullyReduceHauntChance).getAsInt();
+			minutesToIncreaseHauntLevelBy1 = Presence.config.getOrSetValue("minutesToIncreaseHauntLevelBy1", minutesToIncreaseHauntLevelBy1).getAsInt();
+			hauntLevelCalculationDelay = Presence.config.getOrSetValue("hauntLevelCalculationDelay", hauntLevelCalculationDelay).getAsInt();
 		} catch (UnsupportedOperationException e) {
 			Presence.LOGGER.error("Configuration issue for PlayerData.java. Wiping and using default.", e);
 			Presence.config.wipe();
@@ -68,10 +75,10 @@ public class PlayerData {
 	private String playerDataPath;
 	private LocalDateTime joinTime;
 	private boolean isHaunted = false;
+	private float hauntLevel = defaultHauntLevel;	// Divides delay minima and maxima by hauntLevel, such that events happen more often as time goes on. 1.0 has no effect, and larger numbers increase events
 
 	// Persistant
 	private float hauntChance = defaultHauntChance;	// Chance of being haunted when joining the server
-	private float hauntLevel = defaultHauntLevel;	// Divides delay minima and maxima by hauntLevel, such that events happen more often as time goes on. 1.0 has no effect, and larger numbers increase events
 	private long playTime = 0;			// Time in minutes that the player has played
 
 	private PlayerData(final PlayerEntity playerEntity) {
@@ -81,12 +88,6 @@ public class PlayerData {
 		joinTime = LocalDateTime.now();
 		load();
 		rollHauntChance();
-	}
-
-	public void startEvents() {
-		if (isHaunted) {
-			scheduleEvents();
-		}
 	}
 
 	public float getHauntChance() {
@@ -116,36 +117,66 @@ public class PlayerData {
 	}
 
 	public float calculateHauntChance() {
-		final float difference = ((float) (playTime + Duration.between(joinTime, LocalDateTime.now()).toMinutes())) / Math.max(1.0f, minutesToFullyReduceHauntChance);
-		if (isHaunted && hauntChance > hauntChanceMaxBeforeDecrease) {
-			// Decrease haunt chace
-			hauntChance = Math.max(
-				defaultHauntChance,
-				Math.min(1.0f, hauntChance - difference)
-			);
+		if (isHaunted && hauntChance > hauntChanceMaxBeforeReset) {
+			// Reset haunt chance
+			hauntChance = defaultHauntChance;
 		} else {
-			// Incraese haunt chace
+			// Incraese haunt chace based on instance playtime
 			hauntChance = Math.max(
 				defaultHauntChance,
-				Math.min(1.0f, hauntChance + difference)
+				Math.min(
+					1.0f,
+					hauntChance + (
+						((float) (Duration.between(joinTime, LocalDateTime.now()).toMinutes())) / Math.max(1, minutesToFullyReduceHauntChance)
+					)
+				)
 			);
 		}
 		return hauntChance;
 	}
 
+	public float calculateHauntLevel() {
+		if (isHaunted) {
+			// Increase haunt level
+			hauntLevel = Math.max(
+				0.001f,
+				(defaultHauntLevel + (
+					((float) Duration.between(joinTime, LocalDateTime.now()).toMinutes()) / Math.max(1, minutesToIncreaseHauntLevelBy1)
+				))
+			);
+		}
+		return hauntLevel;
+	}
+
+	public void startEvents() {
+		if (isHaunted) {
+			scheduleEvents();
+		}
+	}
+
 	public void scheduleEvents() {
 		final PlayerEntity player = getPlayer();
+		scheduleHauntLevelCalculation();
 		if (AmbientSounds.ambientSoundsEnabled) AmbientSounds.scheduleEvent(player);
 		if (ExtinguishTorches.extinguishTorchesEnabled) ExtinguishTorches.scheduleTracking(player);
 		if (Footsteps.footstepsEnabled) Footsteps.scheduleEvent(player);
 		if (NearbySounds.nearbySoundsEnabled) NearbySounds.scheduleEvent(player);
 	}
 
+	private void scheduleHauntLevelCalculation() {
+		Events.scheduler.schedule(
+			() -> {
+				calculateHauntLevel();
+				if (!player.isRemoved()) scheduleHauntLevelCalculation();
+			},
+			hauntLevelCalculationDelay, TimeUnit.SECONDS
+		);
+	}
+
 	private void load() {
 		final JsonFile dataFile = new JsonFile(playerDataPath);
 		try {
 			hauntChance = dataFile.getOrSetValue("hauntChance", hauntChance).getAsFloat();
-			hauntLevel = dataFile.getOrSetValue("hauntLevel", hauntLevel).getAsFloat();
 			playTime = dataFile.getOrSetValue("playTime", playTime).getAsLong();
 		} catch (UnsupportedOperationException e) {
 			Presence.LOGGER.error("Corrupted player data for " + uuid.toString() + ". Wiping and using default values.", e);
@@ -164,7 +195,6 @@ public class PlayerData {
 		final JsonFile dataFile = new JsonFile(playerDataPath);
 		final JsonObject data = dataFile.getJsonObject();
 		dataFile.setValue(data, "hauntChance", hauntChance);
-		dataFile.setValue(data, "hauntLevel", hauntLevel);
 		dataFile.setValue(data, "playTime", playTime + Duration.between(joinTime, LocalDateTime.now()).toMinutes());
 		dataFile.writeJson(data, false);
 	}
