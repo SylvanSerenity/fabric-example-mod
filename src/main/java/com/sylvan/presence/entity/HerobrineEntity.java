941dab63f823e666e5dda2c5a26fe8335eea39e6
Avery Aaron <sylvanserene@gmail.com>
2024-02-18 20:41:36 -0600

    Added head rotation

--- src/main/java/com/sylvan/presence/entity/HerobrineEntity.java
+++ src/main/java/com/sylvan/presence/entity/HerobrineEntity.java
@@ -10,6 +10,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtFloat;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.EulerAngle;
 import net.minecraft.world.World;
 
 public class HerobrineEntity {
@@ -35,7 +36,6 @@ public class HerobrineEntity {
 		headBodyCompound.putBoolean("Invisible", true);
 		headBodyCompound.putBoolean("Invulnerable", true);
 		headBodyCompound.putBoolean("NoBasePlate", true);
-		headBodyCompound.putBoolean("NoGravity", true);
 		headBodyCompound.putBoolean("ShowArms", true);
 		headBodyCompound.putInt("DisabledSlots", 2039583);
 		headBodyCompound.put("Pose", armLegPoseCompound);
@@ -43,7 +43,6 @@ public class HerobrineEntity {
 		legsCompound.putBoolean("Invisible", true);
 		legsCompound.putBoolean("Invulnerable", true);
 		legsCompound.putBoolean("NoBasePlate", true);
-		legsCompound.putBoolean("NoGravity", true);
 		legsCompound.putBoolean("ShowArms", true);
 		legsCompound.putInt("DisabledSlots", 2039583);
 		legsCompound.putBoolean("Small", true);
@@ -52,6 +51,7 @@ public class HerobrineEntity {
 
 	public HerobrineEntity(final World world, final BlockPos blockPos, final float yaw, final float pitch) {
 		this.world = world;
+		EulerAngle headRotation = new EulerAngle(pitch, yaw, 0.1f);
 
 		ItemStack head = newModelItem(1350146);
 		ItemStack eyes = newModelItem(1350147);
@@ -66,22 +66,28 @@ public class HerobrineEntity {
 		headArmsEntity.equipStack(EquipmentSlot.HEAD, head);
 		headArmsEntity.equipStack(EquipmentSlot.MAINHAND, rightArm);
 		headArmsEntity.equipStack(EquipmentSlot.OFFHAND, leftArm);
+		headArmsEntity.setNoGravity(true);
 		headArmsEntity.refreshPositionAndAngles(blockPos, yaw, pitch);
+		headArmsEntity.setHeadRotation(headRotation);
 
 		this.eyesEntity = EntityType.ARMOR_STAND.create(world);
 		eyesEntity.readCustomDataFromNbt(headBodyCompound);
 		eyesEntity.equipStack(EquipmentSlot.HEAD, eyes);
+		eyesEntity.setNoGravity(true);
 		eyesEntity.refreshPositionAndAngles(blockPos, yaw, pitch);
+		eyesEntity.setHeadRotation(headRotation);
 
 		this.bodyEntity = EntityType.ARMOR_STAND.create(world);
 		bodyEntity.readCustomDataFromNbt(headBodyCompound);
 		bodyEntity.equipStack(EquipmentSlot.HEAD, body);
+		bodyEntity.setNoGravity(true);
 		bodyEntity.refreshPositionAndAngles(blockPos, yaw, pitch);
 
 		this.legsEntity = EntityType.ARMOR_STAND.create(world);
 		legsEntity.readCustomDataFromNbt(legsCompound);
 		legsEntity.equipStack(EquipmentSlot.MAINHAND, rightLeg);
 		legsEntity.equipStack(EquipmentSlot.OFFHAND, leftLeg);
+		legsEntity.setNoGravity(true);
 		legsEntity.refreshPositionAndAngles(blockPos, yaw, pitch);
 	}
 
